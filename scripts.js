50efb3da37429ff9828ea236c939d61b4ea081f8
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-23 17:53:49 -0400

    Complete exercises.

--- scripts.js
+++ scripts.js
@@ -8,35 +8,42 @@ var brunchFoods = [
 ];
 
 // 1. Access the first item in the brunchFoods array. 
-
+console.log(brunchFoods[0]);
 
 
 // 2. Replace the first item in the array with your favorite brunch food. 
+brunchFoods[0] = 'Scrambled eggs with cheese';
 
 
 
 // 3. Check the value of brunchFoods
+console.log(brunchFoods);
 
 
 
 // 4. Add (don't replace) a value to the brunchFoods array.
-
+brunchFoods.push('Waffles');
 
 
 // 5. Check the value of brunchFoods
+console.log(brunchFoods);
 
 
 
 // 6. Create a new variable named favoriteBrunchFood and set it equal to a string of your favorite brunch food (you can do this right underneath the brunchFoods array!). 
-
+let favoriteBrunchFoods = 'Left over pizza';
 
 
 // 7. Check the value of favoriteBrunchFood
-
+console.log(favoriteBrunchFoods);
 
 
 // 8. Write an if conditional that checks if the first item in the brunchFoods array is equal to the value of your favoriteBrunchFood. If we have a match, log "Yay, I'm having my favorite food for brunch!" If we do NOT have a match, log "I guess this brunch is fine.";
-
+if (brunchFoods[0] === favoriteBrunchFoods) {
+  console.log("Yay, I'm having my favorite food for brunch!");
+} else {
+  console.log("I guess this brunch is fine.");
+}
 
 
 // 9. Finish the function below that will REPLACE the first item in our brunchFoods array with whatever you pass into the function. Make sure to return the brunchFoods array to test that your code works as expected. 
